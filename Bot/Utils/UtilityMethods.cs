9454b020a02d7f83cba7112cae3451d5b74de669
Zachary Austin <Zachary@admiralfeb.net>
2022-02-24 02:11:07 -0600

    add a throw/break for periodicasync

--- Bot/Utils/UtilityMethods.cs
+++ Bot/Utils/UtilityMethods.cs
@@ -9,6 +9,7 @@ public static class UtilityMethods
         {
             await action();
             await timer.WaitForNextTickAsync(cancellationToken);
+            cancellationToken.ThrowIfCancellationRequested();
         }
     }
 }
